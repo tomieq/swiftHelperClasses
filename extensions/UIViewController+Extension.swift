aeefa30540666b61d8fc6175f5631876901020ec
tomieq <tomieq@wp.pl>
2020-12-04 17:10:36 +0100

    Create UIViewController+Extension.swift

---
+++ extensions/UIViewController+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Extension.swift
+//
+//  Created by tomieq on 13.06.2017.
+//  Copyright © 2017 tomieq. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
